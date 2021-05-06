18a6432f7eaa581d373d65f5e516601b5ebe53c7
Pu Anlai <inspectormustache@users.noreply.github.com>
2021-05-06 21:52:24 +0200

    Reboot only if efibootmgr finishes succesfully

--- .config/fish/functions/bootwin.fish
+++ .config/fish/functions/bootwin.fish
@@ -1,5 +1,5 @@
 function bootwin
     set -l win_entry (efibootmgr | perl -ne 'print if s/^Boot(\d+).*Windows Boot Manager/\1/')
     command sudo efibootmgr -n $win_entry
-    command systemctl reboot
+    and command systemctl reboot
 end
