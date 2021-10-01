607cf3da3a2ea918656f91bb02e084b2ba407ab9
Pu Anlai <inspectormustache@users.noreply.github.com>
2021-10-01 13:52:10 +0200

    Add fish function rnm.fish

---
+++ .config/fish/functions/rnm.fish
@@ -0,0 +1,44 @@
+function rnm
+    argparse 'n/dry-run' -- $argv
+    set rnm_file (mktemp)
+    set file_error 0
+
+    for file in $argv
+        if [ ! -f "$file" ]
+            echo "No file $file."
+            set file_error 1
+        else
+            echo "$file" >> "$rnm_file"
+        end
+    end
+
+    if [ $file_error -eq 0 ]
+        $EDITOR "$rnm_file"
+
+        if [ ! (wc -l "$rnm_file" | cut -f1 -d\ ) -eq (count $argv) ]
+            echo "Number of file names changed."
+            rm $rnm_file
+            return 1
+        end
+
+        set line_num 1
+        while read -l new_fname
+            set fname "$argv[$line_num]"
+            set line_num (math $line_num + 1)
+
+            if [ -n "$_flag_n" ]
+                echo "$fname --> $new_fname"
+            else if [ "$fname" = "$new_fname" ]
+                :
+            else if [ -f "$new_fname" ]
+                echo "File \"$new_fname\" already exists. Skipping..."
+            else
+                mv -vn -- "$fname" "$new_fname"
+            end
+
+        end < $rnm_file
+    end
+
+    rm $rnm_file
+    return $file_error
+end
