253ef6b302f152a44efb5b42706aa3cf269d137e
Pu Anlai <inspectormustache@users.noreply.github.com>
2021-10-25 13:59:56 +0200

    Handle empty directories in cdp

--- .config/fish/functions/cdp.fish
+++ .config/fish/functions/cdp.fish
@@ -2,8 +2,9 @@ function cdp -d "Browse through the file system with fzf."
     set orig_destination (pwd)
     true
     while true
-        printf "%s\n" * .. | fzf --preview="echo -e \"\e[1;37m\"{}\"\e[0m\"; if [ -d {} ]; then ls {}; elif grep -qI '' {}; then head -n \$((\$FZF_PREVIEW_LINES - 1)) {}; else file {} | fold -s -w \$FZF_PREVIEW_COLUMNS; fi" \
-                              --bind="ctrl-c:execute(cd \"$orig_destination\")+abort" | read selection
+        set files *
+        printf "%s\n" $files .. | fzf --preview="echo -e \"\e[1;37m\"{}\"\e[0m\"; if [ -d {} ]; then ls {}; elif grep -qI '' {}; then head -n \$((\$FZF_PREVIEW_LINES - 1)) {}; else file {} | fold -s -w \$FZF_PREVIEW_COLUMNS; fi" \
+            --bind="ctrl-c:execute(cd \"$orig_destination\")+abort" | read selection
         if [ -d "$selection" ]
             cd $selection
         else if [ -f "$selection" ]
