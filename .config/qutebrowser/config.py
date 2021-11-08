9c0f40d3b2deb3c99f15f13ef8d08b19d6efa53e
Pu Anlai <inspectormustache@users.noreply.github.com>
2021-11-08 11:18:01 +0100

    Change qutebrowser start pages

--- .config/qutebrowser/config.py
+++ .config/qutebrowser/config.py
@@ -327,7 +327,7 @@ c.aliases[
 
 ## Miscellaneous settings
 # Start page
-c.url.start_pages = choice(start_pages)
+c.url.start_pages = [choice(start_pages), "https://www.amd.com/de/direct-buy/de"]
 # Where to show the downloaded files.
 c.downloads.position = "bottom"
 # Open new tabs in background
