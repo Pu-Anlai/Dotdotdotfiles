00617e606a7f6c4e26bd4ff2dcad19261eec0073
Pu Anlai <inspectormustache@users.noreply.github.com>
2021-12-10 18:15:42 +0100

    Activate hints in alacritty

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -38,11 +38,14 @@ function fish_user_key_bindings
     bind -M insert \eo 'mkdir -p (string replace -r \'^~\' $HOME -- (commandline -t))'
 
     # update history but keep commandline
-    bind -M insert \e\cH 'history merge'
+    bind -M insert \cH 'history merge'
 
     # clear terminal screen
-    bind -M insert \e\cL 'cls; commandline -f repaint'
+    bind -M insert \cX 'cls; commandline -f repaint'
 
     # toggle shadow mode
     bind -M insert \e\? 'if set -q fish_private_mode; exec fish; else; exec fish --private; end'
+
+    # unbind Ctrl+L for alacritty
+    bind --erase -M insert --preset \cL
 end
