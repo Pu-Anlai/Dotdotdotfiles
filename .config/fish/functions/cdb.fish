05b85d2e5b135763057f8f8202e85f45bf7c708f
Pu Anlai <inspectormustache@users.noreply.github.com>
2022-01-05 18:05:42 +0100

    Use file instead of uvar for cdb.fish

--- .config/fish/functions/cdb.fish
+++ .config/fish/functions/cdb.fish
@@ -1,37 +1,28 @@
 function cdb -d "Browse through directory bookmarks."
     argparse -n cdb 'a/add' 'd/delete' -- $argv
+    set bookmark_file "$__fish_user_data_dir/cdb_bookmarks"
 
     if [ -n "$_flag_a" ]
         # add bookmark
-        if [ (count $argv) -ne 1 ]
-            echo "Wrong number of arguments."
-            return 1
+        for location in $argv
+            echo $location >> "$bookmark_file"
         end
-
-        set -Ua FISH_CDB_BOOKMARKS (realpath $argv[1])
         return
     else if [ -n "$_flag_d" ]
         # delete bookmark
-        if [ (count $argv) -ne 1 ]
-            echo "Wrong number of arguments."
-            return 1
+        for location in $argv
+            set line_num (awk -v line="$location" '$0 == line {print NR}' "$bookmark_file")
+            test -z $line_num && echo "$location not in bookmark file." && return 1
+            sed -i {$line_num}d "$bookmark_file"
         end
-
-        set bm (realpath $argv[1])
-        set index (contains -i $bm $FISH_CDB_BOOKMARKS)
-        or begin
-            echo "Bookmark $bm does not exist."
-            return 1
-        end
-        
-        set -Ue FISH_CDB_BOOKMARKS[$index]
         return
     end
 
     # no bookmarks?
-    any $FISH_CDB_BOOKMARKS || return 1
+    test -f "$bookmark_file" || return 1
+    test (stat --printf='%s' "$bookmark_file") -gt 0 || return 1
 
-    printf '%s\n' $FISH_CDB_BOOKMARKS | \
+    cat "$bookmark_file" | \
         fzf --expect=ctrl-alt-m,return --with-nth -1 -d "/" +m --preview="if [ -e {} ]; then echo -e \"\e[1;37m\$(basename {})\e[0m\"; ls {}; else echo \"Directory doesn't exist.\"; fi" | \
         read -L exit_key directory
     if [ -e $directory ]
