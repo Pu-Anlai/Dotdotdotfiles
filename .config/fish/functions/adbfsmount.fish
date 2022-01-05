5d59172cc3fc8c44ca229d83faa2b37665040c7f
Pu Anlai <inspectormustache@users.noreply.github.com>
2022-01-05 18:23:59 +0100

    Remove universal variables from adbfsmount.fish

--- .config/fish/functions/adbfsmount.fish
+++ .config/fish/functions/adbfsmount.fish
@@ -1,12 +1,19 @@
 function adbfsmount --description "Provide a adbfs mountpoint as $adbfsmnt"
-    if [ -d "$adbfsmnt" ]
-        set_color -o; echo "Unmounting \$adbfsmnt ($adbfsmnt)..."; set_color normal
-        fusermount -u "$adbfsmnt"
-        and set -eU adbfsmnt
+    set adbfs (findmnt -rnt fuse.adbfs | head -n1 | cut -d\  -f1)
+
+    if [ -n "$adbfs" ]
+        if set -qg adbfsmnt
+            set_color -o; echo "Unmounting \$adbfsmnt ($adbfsmnt)..."; set_color normal
+            fusermount -u "$adbfsmnt"
+        else
+            set_color -o; echo "Setting \$adbfsmnt to $adbfs..."; set_color normal
+            set -g adbfsmnt "$adbfs"
+        end
+
     else
-        set -U adbfsmnt (mktemp -d --suffix "-adbfs")
+        set -g adbfsmnt (mktemp -d --tmpdir 'adbfs.XXXXXX')
         set_color -o; echo "Mounting under $adbfsmnt (\$adbfsmnt)..."; set_color normal
         adbfs "$adbfsmnt"
-        or set -eU adbfsmnt
+        or set -eg adbfsmnt
     end
 end
