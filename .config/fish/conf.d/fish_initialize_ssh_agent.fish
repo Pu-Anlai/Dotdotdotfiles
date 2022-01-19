f1348fa746fe6199c8c133edf22d6f27d29cee7a
Pu Anlai <inspectormustache@users.noreply.github.com>
2022-01-19 20:05:47 +0100

    Fix ssh agent startup conditions

--- .config/fish/conf.d/fish_initialize_ssh_agent.fish
+++ .config/fish/conf.d/fish_initialize_ssh_agent.fish
@@ -1,5 +1,5 @@
 # if ssh isn't installed/set-up, do nothing
-if not command -s ssh 1>&- 2>&-; or not test -e $HOME/.ssh
+if command -s ssh 1>&- 2>&- && test -e "$HOME/.ssh"
     test -z "$SSH_ENV" && set -xg SSH_ENV $HOME/.ssh/environment
     not __ssh_agent_is_started && __ssh_agent_start
 end
