b0f93d698fd746ff04b09ff4a9b9a3d1ca0ef470
Pu Anlai <inspectormustache@users.noreply.github.com>
2022-02-14 20:15:15 +0100

    Add °evil-org-meta-open-above/-below commands

--- .config/emacs/subinits/init-org-mode.el
+++ .config/emacs/subinits/init-org-mode.el
@@ -1,7 +1,32 @@
 ;; -*- lexical-binding: t -*-
 (use-package org
   :commands org-mode
+  :general
+  (:states          'normal
+   :keymaps         'org-mode-map
+   "o"              '°evil-org-meta-open-below
+   "O"              '°evil-org-meta-open-above)
+  (general-leader
+    :states         'normal
+    :keymaps        'org-mode-map
+    "o"             'evil-open-below
+    "O"             'evil-open-above)
   :config
+  (defun °evil-org-meta-open-below (count)
+    (interactive "p")
+    (if (org-in-item-p)
+        (org-end-of-item)
+        (end-of-line))
+    (org-meta-return)
+    (evil-insert count))
+  (defun °evil-org-meta-open-above (count)
+    (interactive "p")
+    (forward-line -1)
+    (if (org-in-item-p)
+        (org-end-of-item)
+      (end-of-line))
+    (org-meta-return)
+    (evil-insert count))
   (setq org-log-done 'time)
   (add-hook 'org-mode-hook #'visual-line-mode))
 
