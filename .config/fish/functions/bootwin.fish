16484da4a749012707696d86796f1b2e912dc674
Pu Anlai <inspectormustache@users.noreply.github.com>
2022-07-12 13:34:49 +0200

    Simplify bootwin function

--- .config/fish/functions/bootwin.fish
+++ .config/fish/functions/bootwin.fish
@@ -1,5 +1,5 @@
 function bootwin
-    set -l win_entry (efibootmgr | perl -ne 'print if s/^Boot(\d+).*Windows Boot Manager/\1/')
+    set -l win_entry (efibootmgr | grep "Windows Boot Manager" | cut -f1 -d\* | tr -d [:alpha:])
     command sudo efibootmgr -n $win_entry
     and command systemctl reboot
 end
