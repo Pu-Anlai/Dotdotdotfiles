225111545f1ba3305c85b752e124569e2dd454e1
Pu Anlai <inspectormustache@users.noreply.github.com>
2023-08-14 17:37:49 +0200

    Towards perfection in the °org-meta-open functions

--- .config/emacs/subinits/init-org-mode.el
+++ .config/emacs/subinits/init-org-mode.el
@@ -6,10 +6,8 @@
    :keymaps         'org-mode-map
    "("              'org-backward-element
    ")"              'org-forward-element
-    "o"             '°org-meta-open-below
-    "O"             '°org-meta-open-above
-    "M-o"           '°org-meta-ctrl-open-below
-    "M-O"           '°org-meta-ctrl-open-above)
+   "o"              '°org-meta-open-below
+   "O"              '°org-meta-open-above)
   (general-leader
     :states         'normal
     :keymaps        'org-mode-map
@@ -25,14 +23,18 @@
   (general-goleader
     :states         'motion
     :keymaps        'org-mode-map
-    "A"             'org-agenda)
+    "A"             'org-agenda
+    "o"             '°org-meta-ctrl-open-below
+    "O"             '°org-meta-ctrl-open-above
+    "L"             'org-toggle-link-display)
   ;; keybindings for agenda-view
   (:states          'normal
    :keymaps         'org-agenda-mode-map
    "("              'org-agenda-earlier
    ")"              'org-agenda-later
    "r"              'org-agenda-redo
-   "q"              'org-agenda-quit)
+   "q"              'org-agenda-quit
+   "RET"            (general-l (org-agenda-show-1 3)))
   (general-leader
     :states         'normal
     :keymaps        'org-agenda-mode-map
@@ -41,25 +43,42 @@
   (evil-set-initial-state 'org-agenda-mode 'normal)
   (evil-collection-org-setup)
   (setq org-log-done 'time
-        org-adapt-indentation t)
+        org-adapt-indentation t
+        org-cycle-separator-lines -1)
 
   (defun °org-meta-open-below ()
-  (interactive)
-  (if (org-at-item-p)
-      (org-end-of-item)
-    (org-end-of-line))
-    (while (org-fold-folded-p)
-      (forward-char))
-    (org-meta-return)
-  (evil-insert 1))
+    (interactive)
+    (let ((context-func (cond ((org-at-table-p)
+                               (call-interactively #'org-table-end-of-field)
+                               #'org-table-wrap-region)
+                              ((org-at-item-p)
+                               (org-end-of-item)
+                               #'org-insert-item)
+                              ((org-at-heading-p)
+                               (org-end-of-line)
+                               #'org-insert-heading-respect-content)
+                              (t
+                               (lambda () (interactive) (evil-insert-newline-below))))))
+      (while (org-fold-folded-p)
+        (forward-char))
+      (call-interactively context-func)
+      (indent-according-to-mode)
+      (evil-insert 1)))
 
   (defun °org-meta-open-above ()
     (interactive)
-    (if (org-at-item-p)
-        (org-beginning-of-item)
-      (org-beginning-of-line))
-    (org-meta-return)
-    (evil-insert 1))
+    (let ((context-func
+           (cond ((org-at-table-p)
+                  (call-interactively #'org-table-beginning-of-field)
+                  #'org-table-insert-row)
+                 ((org-at-item-p)
+                  (org-beginning-of-item)
+                  #'org-insert-item)
+                 (t
+                  (org-beginning-of-line)
+                  #'org-meta-return))))
+    (call-interactively context-func)
+    (evil-insert 1)))
 
   (defun °org-meta-ctrl-open-below ()
     (interactive)
