1fdb585d1806b892ea85a67fcc0a86c7e41a84b8
Pu Anlai <inspectormustache@users.noreply.github.com>
2023-11-01 16:26:54 +0100

    Implement archiving in org-mode

--- .config/emacs/subinits/init-org-mode.el
+++ .config/emacs/subinits/init-org-mode.el
@@ -24,7 +24,9 @@
     "L"             'org-metaright
     "J"             'org-metadown
     "K"             'org-metaup
-    "X"             'org-toggle-checkbox)
+    "x"             'org-toggle-checkbox
+    "X"             '°org-toggle-checkbox-presence
+    "p"             'org-set-property)
   (general-goleader
     :states         'motion
     :keymaps        'org-mode-map
@@ -53,6 +55,12 @@
         org-adapt-indentation t
         org-cycle-separator-lines -1)
 
+  (dolist (action #'(org-forward-sentence
+                     org-backward-sentence
+                     °org-prev-element
+                     °org-next-element))
+    (evil-declare-not-repeat action))
+  
   (dolist (func
            #'(°°org-visual-line-mode
               org-fold-hide-drawer-all))
@@ -130,15 +138,53 @@
                   "TODO"
               (car org-todo-keywords-1))))
 
-  (defun °°org-undone-children-at-point-p ()
+  (defun °°org-undone-children-at-point-p (&optional no-children)
+    "Return t if we are at a heading with children that are not in the
+`org-done-keywords' list. If NO-CHILDREN is non-nil, also return t if there are
+no children at all."
     (save-excursion
-      (or
-       (not (org-at-heading-p))
-       (org-goto-first-child)
-       (while (and
-               (member (org-get-todo-state) org-done-keywords)
-               (org-get-next-sibling))))))
+      (catch 'childless
+        (and
+         (org-at-heading-p)
+         (if (org-goto-first-child)
+             t
+           (throw 'childless no-children))
+         (let ((level (org-current-level)))
+           (while (and
+                   (member (org-get-todo-state) org-done-keywords)
+                   (org-get-next-sibling)))
+           (>= (org-current-level) level))))))
   
+  (defun °org-process-done?-headings ()
+    (interactive)
+    (org-map-entries (lambda ()
+                       (when (not (°°org-undone-children-at-point-p t))
+                         (org-todo)))
+                     (concat "/" (string-join org-not-done-keywords "|"))))
+
+  (defun °org-move-done-headings-to-archive (prefix)
+    (interactive "P")
+    (let ((count 0))
+      (org-map-entries
+       (if prefix
+           (lambda ()
+             (setq count (°°org-archive-heading-if-all-done count t)))
+         (lambda ()
+           (setq count (°°org-archive-heading-if-all-done count))))
+       (concat "/" (string-join org-done-keywords "|")))
+      (message "%s subtrees archived." count)))
+
+  (defun °°org-archive-heading-if-all-done (count &optional confirm)
+    (unless (°°org-undone-children-at-point-p)
+      (setq org-map-continue-from (org-element-property :begin (org-element-at-point)))
+      (unless (and confirm (not (y-or-n-p (concat (org-get-heading) " - archive this entry? "))))
+       (org-archive-subtree-default)
+       (+ count 1))))
+  
+  (defun °org-toggle-checkbox-presence ()
+    (interactive)
+    (let ((current-prefix-arg '(4)))
+      (call-interactively #'org-toggle-checkbox)))
 
   (defun °°org-visual-line-mode ()
     (visual-line-mode)
