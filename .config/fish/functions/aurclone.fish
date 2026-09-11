245866e4cc95d28b8097786cb510e07e90402c50
Pu Anlai <inspectormustache@users.noreply.github.com>
2026-09-12 00:40:17 +0200

    fish: Test if pkg exists before cloning in aurclone

--- .config/fish/functions/aurclone.fish
+++ .config/fish/functions/aurclone.fish
@@ -1,8 +1,16 @@
 function aurclone
     argparse 'd/directory=' -- $argv
+    test (count $argv) -eq 1 || return 1
+    set pkg $argv[1]
+
+    if [ -z "$(git ls-remote "https://aur.archlinux.org/$pkg.git")" ]
+        echo "Package $pkg not found in AUR." >&2
+        return 1
+    end
+
     if set -q _flag_directory
-        git clone "https://aur.archlinux.org/$argv[1].git" $_flag_directory
+        git clone "https://aur.archlinux.org/$pkg.git" $_flag_directory || return 1
     else
-        git clone "https://aur.archlinux.org/$argv[1].git"
+        git clone "https://aur.archlinux.org/$pkg.git" || return 1
     end
 end
